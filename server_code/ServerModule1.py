51628af1ca1f592164f33749f65856daaeb9df79
elrohir.ciryatan@gmail.com <elrohir.ciryatan@gmail.com>
2021-11-04 14:44:19 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -37,4 +37,5 @@ def update_generate_input_templ_name(templ_choice_id):
     
 @anvil.server.callable
 def get_input_templ_list():
-  return set(row['template_id'] for row in app_tables.temp_template.search())
+  print(list(row['template_name'] for row in app_tables.temp_template.search()))
+  return list(row['template_name'] for row in app_tables.temp_template.search())
