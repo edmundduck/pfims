77c56152970d247341ff58573a62e8f6ff419824
elrohir.ciryatan@gmail.com <elrohir.ciryatan@gmail.com>
2021-11-05 00:22:40 +0000

    Edited form_main.form_sub1_input, settings and form_main.form_sub1_input.RowTemplate1

--- client_code/form_main/form_sub1_input/__init__.py
+++ client_code/form_main/form_sub1_input/__init__.py
@@ -11,37 +11,43 @@ class form_sub1_input(form_sub1_inputTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    self.input_templ_name.text = anvil.server.call('update_generate_input_templ_name', self.input_dropdown_templ.selected_value)
+    self.input_templ_name.text = anvil.server.call('update_generate_input_templ_name', 
+                                                   self.input_dropdown_templ.selected_value)
     
     
   def input_button_plus_click(self, **event_args):
     """This method is called when the button is clicked"""
-    if self.input_chk_dtl.checked is True:
-      anvil.server.call('add_temp_input', 
-                        date_sales=self.input_date.date, 
-                        date_buy=self.input_date.date, 
-                        symbol=self.input_symbol.text, 
-                        qty=self.input_qty.text, 
-                        sales=self.input_sales.text, 
-                        cost=self.input_cost.text, 
-                        pnl=self.input_pnl.text, 
-                        sell_price=0, 
-                        buy_price=0)
-    else:
-      anvil.server.call('add_temp_input', 
-                        date_sales=self.input_date.date, 
-                        date_buy=self.input_date.date, 
-                        symbol=self.input_symbol.text, 
-                        qty=self.input_qty.text, 
-                        sales=self.input_sales.text, 
-                        cost=self.input_cost.text, 
-                        pnl=self.input_pnl.text, 
-                        sell_price=0, 
-                        buy_price=0)      
+#    anvil.server.call('add_temp_input', 
+#                      sell_date=self.input_selldate.date, 
+#                      buy_date=self.input_buydate.date, 
+#                      symbol=self.input_symbol.text, 
+#                      qty=self.input_qty.text, 
+#                      sales=self.input_sales.text, 
+#                      cost=self.input_cost.text, 
+#                      pnl=self.input_pnl.text, 
+#                      sell_price=self.input_sales.text/qty, 
+#                      buy_price=self.input_cost.text/qty)
+    self.input_sell_price.text = self.input_sales.text / self.input_qty.text
+    self.input_buy_price.text = self.input_cost.text / self.input_qty.text
+    
+    self.new_data = {"sell_date": self.input_selldate.date,
+                    "buy_date": self.input_buydate.date,
+#                    "symbol": self.input_symbol.text,
+                    "symbol": "XXX",
+                    "qty": self.input_qty.text,
+                    "sales": self.input_sales.text,
+                    "cost": self.input_cost.text,
+                    "pnl": self.input_pnl.text,
+                    "sell_date": self.input_sell_price.text,
+                    "buy_date": self.input_buy_price.text}
 
+    #self.data = self.input_repeating_panel.items
+    self.input_repeating_panel.items = [self.new_data]
+    
   def input_dropdown_templ_change(self, **event_args):
     """This method is called when an item is selected"""
-    self.input_templ_name.text = anvil.server.call('update_generate_input_templ_name', self.input_dropdown_templ.selected_value)
+    self.input_templ_name.text = anvil.server.call('update_generate_input_templ_name', 
+                                                   self.input_dropdown_templ.selected_value)
 
   def input_dropdown_templ_show(self, **event_args):
     """This method is called when the DropDown is shown on the screen"""
