3d1703365f9d5deaefdd73474d8705a5bc9632fa
elrohir.ciryatan@gmail.com <elrohir.ciryatan@gmail.com>
2021-11-05 11:47:24 +0000

    Input_form_save_draft_ok

--- client_code/form_main/form_sub1_input/__init__.py
+++ client_code/form_main/form_sub1_input/__init__.py
@@ -11,38 +11,36 @@ class form_sub1_input(form_sub1_inputTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    self.input_templ_name.text = anvil.server.call('update_generate_input_templ_name', 
-                                                   self.input_dropdown_templ.selected_value)
     
+    # Initiate repeating panel items to an empty list otherwise will throw NoneType error
+    self.input_repeating_panel.items = []
+
+    # Debug - populate test data - START
+    self.input_selldate.date = "2021-05-10"
+    self.input_buydate.date = "2021-04-19"
+    self.input_symbol.text = "AMD"
+    self.input_qty.text = 1000
+    self.input_sales.text = 10000
+    self.input_cost.text = 8000
+    self.input_pnl.text = 2000
+    # Debug - populate test data - END
     
   def input_button_plus_click(self, **event_args):
     """This method is called when the button is clicked"""
-#    anvil.server.call('add_temp_input', 
-#                      sell_date=self.input_selldate.date, 
-#                      buy_date=self.input_buydate.date, 
-#                      symbol=self.input_symbol.text, 
-#                      qty=self.input_qty.text, 
-#                      sales=self.input_sales.text, 
-#                      cost=self.input_cost.text, 
-#                      pnl=self.input_pnl.text, 
-#                      sell_price=self.input_sales.text/qty, 
-#                      buy_price=self.input_cost.text/qty)
     self.input_sell_price.text = self.input_sales.text / self.input_qty.text
     self.input_buy_price.text = self.input_cost.text / self.input_qty.text
     
-    self.new_data = {"date_picker_1": self.input_selldate.date,
+    self.new_data = {"sell_date": self.input_selldate.date,
                     "buy_date": self.input_buydate.date,
-#                    "symbol": self.input_symbol.text,
-                    "symbol": "XXX",
+                    "symbol": self.input_symbol.text,
                     "qty": self.input_qty.text,
                     "sales": self.input_sales.text,
                     "cost": self.input_cost.text,
                     "pnl": self.input_pnl.text,
-                    "sell_date": self.input_sell_price.text,
-                    "buy_date": self.input_buy_price.text}
-
-    #self.data = self.input_repeating_panel.items
-    self.input_repeating_panel.items = [self.new_data]
+                    "sell_price": self.input_sell_price.text,
+                    "buy_price": self.input_buy_price.text}
+    
+    self.input_repeating_panel.items = self.input_repeating_panel.items + [self.new_data]
     
   def input_dropdown_templ_change(self, **event_args):
     """This method is called when an item is selected"""
@@ -52,5 +50,28 @@ class form_sub1_input(form_sub1_inputTemplate):
   def input_dropdown_templ_show(self, **event_args):
     """This method is called when the DropDown is shown on the screen"""
     self.input_dropdown_templ.items = anvil.server.call('get_input_templ_list')
+    self.input_templ_name.text = anvil.server.call('get_input_templ_name', 
+                                                   self.input_dropdown_templ.selected_value)
 
+  def button_save_templ_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    
+    templ_id = anvil.server.call('generate_new_templ_id')
+    
+    anvil.server.call('upsert_temp_template',
+                      template_id=templ_id,
+                      template_name=self.input_templ_name.text)
+    
+    for row in self.input_repeating_panel.items:
+      anvil.server.call('upsert_temp_input', 
+                        sell_date=row['sell_date'], 
+                        buy_date=row['buy_date'], 
+                        template_id=templ_id, 
+                        symbol=row['symbol'], 
+                        qty=row['qty'], 
+                        sales=row['sales'], 
+                        cost=row['cost'], 
+                        pnl=row['pnl'], 
+                        sell_price=row['sell_price'], 
+                        buy_price=row['buy_price'])
 
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -2,6 +2,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import random
+import string
+from datetime import date, datetime
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -16,10 +19,16 @@ import anvil.server
 #   return 42
 #
 
+# Static variables
+RANDOM_ID_ALPHA_LEN = 4
+DEFAULT_NEW_TEMPL_TEXT = "[NEW]"
+DEFAULT_NEW_TEMPL_NAME = "NewTemplate"
+
 @anvil.server.callable
-def add_temp_input(date_sales, date_buy, symbol, qty, sales, cost, pnl, sell_price, buy_price):
-  app_tables.temp_input.add_row(date_sales=date_sales,
-                                date_buy=date_buy,
+def upsert_temp_input(sell_date, buy_date, template_id, symbol, qty, sales, cost, pnl, sell_price, buy_price):
+  app_tables.temp_input.add_row(sell_date=sell_date,
+                                buy_date=buy_date,
+                                template_id=template_id, 
                                 symbol=symbol,
                                 qty=qty,
                                 sales=sales,
@@ -27,18 +36,34 @@ def add_temp_input(date_sales, date_buy, symbol, qty, sales, cost, pnl, sell_pri
                                 pnl=pnl,
                                 sell_price=sell_price,
                                 buy_price=buy_price)
-  
+
 @anvil.server.callable
-def update_generate_input_templ_name(templ_choice_str):
-    if templ_choice_str == "[New]":
-      return "NewTemplate"
-    else:
-      templ_choice_id = templ_choice_str[:templ_choice_str.find("-")].strip()
-      row = app_tables.temp_template.get(template_id=int(templ_choice_id))
-      return row['template_name']
+def upsert_temp_template(template_id, template_name):
+  row = app_tables.temp_template.get(template_id=template_id) or app_tables.temp_template.add_row(template_id=template_id,
+                                                                                                  template_name=template_name)
+  currenttime = datetime.now()
+  row['template_create'] = currenttime if row['template_create'] is None else row['template_create']
+  row['template_lastsave'] = currenttime
+
+@anvil.server.callable
+# 4 alphabets + 1 digit = 26^4 * 9 combinations
+def generate_new_templ_id():
+  new_id = ''.join(random.choice(string.ascii_uppercase) for x in range(RANDOM_ID_ALPHA_LEN)) + str(random.randint(0,9))
+  while app_tables.temp_template.get(template_id=new_id) is not None:
+    new_id = ''.join(random.choice(string.ascii_uppercase) for x in range(RANDOM_ID_ALPHA_LEN)) + str(random.randint(0,9))
+  return new_id
+
+@anvil.server.callable
+def get_input_templ_name(templ_choice_str):
+  if templ_choice_str is None or templ_choice_str == DEFAULT_NEW_TEMPL_TEXT:
+    return DEFAULT_NEW_TEMPL_NAME
+  else:
+    templ_choice_id = templ_choice_str[:templ_choice_str.find("-")].strip()
+    row = app_tables.temp_template.get(template_id=templ_choice_id)
+    return row['template_name'] if row is not None else DEFAULT_NEW_TEMPL_NAME
     
 @anvil.server.callable
 def get_input_templ_list():
   content = list(str(row['template_id']) + " - " + row['template_name'] for row in app_tables.temp_template.search())
-  content.insert(0, "[NEW]")
+  content.insert(0, DEFAULT_NEW_TEMPL_TEXT)
   return content
