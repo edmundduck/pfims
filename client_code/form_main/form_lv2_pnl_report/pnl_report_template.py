49972b3140095cf0845069ef6f0002ad2c44928a
elrohir.ciryatan@gmail.com <elrohir.ciryatan@gmail.com>
2021-11-13 23:07:36 +0000

    Edited form_main.form_lv2_pnl_report.pnl_report_template

--- client_code/form_main/form_lv2_pnl_report/pnl_report_template.py
+++ client_code/form_main/form_lv2_pnl_report/pnl_report_template.py
@@ -18,11 +18,13 @@ class pnl_report_template(pnl_report_templateTemplate):
       self.foreground = 'Green'
       
     if self.item['mode'] == global_var.pnl_list_yr_mode():
-      self.button_year_exp.visible = True
-      self.button_mth_exp.visible = False
+      self.button_exp.visible = True
     elif self.item['mode'] == global_var.pnl_list_mth_mode():
-      self.button_year_exp.visible = False
-      self.button_mth_exp.visible = True
+      self.button_exp.visible = False
     elif self.item['mode'] == global_var.pnl_list_day_mode():
-      self.button_year_exp.visible = False
-      self.button_mth_exp.visible = False
+      self.button_exp.visible = False
+
+  def button_exp_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call('update_pnl_list', )
+
