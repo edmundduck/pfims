a35b3557607afb776fc4853b81250b63bd36e6d7
elrohir.ciryatan@gmail.com <elrohir.ciryatan@gmail.com>
2021-11-30 18:18:17 +0000

    Edited mod_input and settings

--- server_code/mod_input.py
+++ server_code/mod_input.py
@@ -128,7 +128,8 @@ def get_input_templ_broker(templ_choice_str):
 @anvil.server.callable
 # Generate template selection dropdown items
 def get_input_templ_list():
-  content = list(merge_templ_id_name(row['template_id'], row['template_name']) for row in app_tables.templates.search())
+  #content = list(merge_templ_id_name(row['template_id'], row['template_name']) for row in app_tables.templates.search())
+  content = list(merge_templ_id_name(row['template_id'], row['template_name']) for row in app_tables.templates.search(submitted=False))
   content.insert(0, DEFAULT_NEW_TEMPL_TEXT)
   return content
 
