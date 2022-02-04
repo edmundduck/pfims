4525233d4f0c41d80c095b5ca5d3928c67345d63
elrohir.ciryatan@gmail.com <elrohir.ciryatan@gmail.com>
2022-02-04 00:49:46 +0000

    Edited form_main.form_lv1_input.input_template, Report.form_lv2_pnl_report.pnl_report_template and Report.form_lv2_tranx_list.tranx_list_template

--- client_code/form_main/form_lv1_input/input_template/__init__.py
+++ client_code/form_main/form_lv1_input/input_template/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from .... import validation
 
 class input_template(input_templateTemplate):
   def __init__(self, **properties):
@@ -35,39 +36,51 @@ class input_template(input_templateTemplate):
 
   def button_save_click(self, **event_args):
     """This method is called when the button is clicked"""
-    self.row_sell_price.text = anvil.server.call('cal_price' ,self.row_sales.text, self.row_qty.text)
-    self.row_buy_price.text = anvil.server.call('cal_price', self.row_cost.text, self.row_qty.text)
-    self.row_pnl.text = anvil.server.call('cal_profit', self.row_sales.text, self.row_cost.text, self.row_fee.text)
-    
-    # Lesson learnt ... THIS LINE DOESN'T WORK!!
-    # new_data = {"sell_date": self.row_selldate.date,
-    #                "buy_date": self.row_buydate.date,
-    #                "symbol": self.row_symbol.text,
-    #                "qty": self.row_qty.text,
-    #                "sales": self.row_sales.text,
-    #                "cost": self.row_cost.text,
-    #                "sell_price": self.row_sell_price.text,
-    #                "buy_price": self.row_buy_price.text,
-    #                "iid": self.row_iid.text}
-    # self.item = self.row_symbol.text
-    # self.item = new_data
-    self.item = {"sell_date": self.row_selldate.date,
-                 "buy_date": self.row_buydate.date,
-                 "symbol": self.row_symbol.text,
-                 "qty": self.row_qty.text,
-                 "sales": float(self.row_sales.text),
-                 "cost": float(self.row_cost.text),
-                 "fee": float(self.row_fee.text),
-                 "sell_price": self.row_sell_price.text,
-                 "buy_price": self.row_buy_price.text,
-                 "pnl": self.row_pnl.text,
-                 "iid": self.row_iid.text}
-    
-    self.input_data_panel_readonly.visible = True
-    self.input_data_panel_editable.visible = False
-    
-    self.parent.raise_event('x-save-change', iid=self.row_iid.text)
-    
+    v = validation.Validator()
+    print(i for i in self.parent.get_components())
+    v.display_when_invalid(self.parent.get_components().valerror_title)
+    v.require_date_field(self.row_selldate, self.valerror_1, True)
+    v.require_date_field(self.row_buydate, self.valerror_2, True)
+    v.require_text_field(self.row_symbol, self.valerror_3, True)
+    v.require_text_field(self.row_qty, self.valerror_4, True)
+    v.require_text_field(self.row_sales, self.valerror_5, True)
+    v.require_text_field(self.row_cost, self.valerror_6, True)
+    v.require_text_field(self.row_fee, self.valerror_7, True)
+
+    if v.is_valid():
+      self.row_sell_price.text = anvil.server.call('cal_price' ,self.row_sales.text, self.row_qty.text)
+      self.row_buy_price.text = anvil.server.call('cal_price', self.row_cost.text, self.row_qty.text)
+      self.row_pnl.text = anvil.server.call('cal_profit', self.row_sales.text, self.row_cost.text, self.row_fee.text)
+      
+      # Lesson learnt ... THIS LINE DOESN'T WORK!!
+      # new_data = {"sell_date": self.row_selldate.date,
+      #                "buy_date": self.row_buydate.date,
+      #                "symbol": self.row_symbol.text,
+      #                "qty": self.row_qty.text,
+      #                "sales": self.row_sales.text,
+      #                "cost": self.row_cost.text,
+      #                "sell_price": self.row_sell_price.text,
+      #                "buy_price": self.row_buy_price.text,
+      #                "iid": self.row_iid.text}
+      # self.item = self.row_symbol.text
+      # self.item = new_data
+      self.item = {"sell_date": self.row_selldate.date,
+                  "buy_date": self.row_buydate.date,
+                  "symbol": self.row_symbol.text,
+                  "qty": self.row_qty.text,
+                  "sales": float(self.row_sales.text),
+                  "cost": float(self.row_cost.text),
+                  "fee": float(self.row_fee.text),
+                  "sell_price": self.row_sell_price.text,
+                  "buy_price": self.row_buy_price.text,
+                  "pnl": self.row_pnl.text,
+                  "iid": self.row_iid.text}
+      
+      self.input_data_panel_readonly.visible = True
+      self.input_data_panel_editable.visible = False
+      
+      self.parent.raise_event('x-save-change', iid=self.row_iid.text)
+      
   def button_delete_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.remove_from_parent()
