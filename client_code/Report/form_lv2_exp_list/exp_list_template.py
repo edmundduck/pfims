c5686c83354c0b8d706b777fb806fdd57ee4323a
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2022-12-20 01:06:47 +0000

    Created Report.form_lv2_exp_list.exp_list_template and Report.form_lv2_exp_list; Edited settings

---
+++ client_code/Report/form_lv2_exp_list/exp_list_template.py
@@ -0,0 +1,30 @@
+from ._anvil_designer import exp_list_templateTemplate
+from anvil import *
+import anvil.users
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+# About amount formatting in design page's data binding field
+# Refer to https://anvil.works/forum/t/formatting-float-fields-in-a-datagrid/6796
+
+class exp_list_template(exp_list_templateTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
+    if self.item['pnl'] < 0:
+      self.foreground = 'Red'
+    else:
+      self.foreground = 'Green'
+
+  def row_link_symbol_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    # TODO - To be implemented with hash routing in the future
+    #newform = form_lv1_settings(symname=self.row_link_symbol.text, 
+    #                    symid=self.row_hidden_iid.text, 
+    #                    symtemplid=self.row_hidden_templ_id.text)
+    #open_form(newform)
+    pass
