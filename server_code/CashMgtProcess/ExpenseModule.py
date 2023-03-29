dfc994e330c3aab9fd4d0831b94e161b07281015
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-03-29 22:41:54 +0000

    Edited Input.AccountMaintForm and CashMgtProcess.ExpenseModule

--- server_code/CashMgtProcess/ExpenseModule.py
+++ server_code/CashMgtProcess/ExpenseModule.py
@@ -35,3 +35,45 @@ def generate_ccy_dropdown():
         cur.close()
     content = list(row['abbv'] + " " + row['name'] + " (" + row['symbol'] + ")" if row['symbol'] else row['abbv'] + " " + row['name'] for row in rows)
     return content
+
+@anvil.server.callable
+# Generate accounts dropdown items
+def generate_accounts_dropdown():
+    conn = sysmod.psqldb_connect()
+    with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+        sql = "SELECT * FROM {schema}.accounts ORDER BY status ASC, valid_to DESC, valid_from DESC"
+        stmt = sql.format(
+            schema=sysmod.schemafin()
+        )
+        cur.execute(stmt)
+        rows = cur.fetchall()
+        cur.close()
+    content = list(row['name'] + " (" + row['id'] + ")" for row in rows)
+    return content
+
+def create_accounts(account):
+    try:
+        conn = sysmod.psqldb_connect()
+        with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+            sql = "INSERT INTO {schema}.accounts (name, ccy, valid_from, valid_to, status) \
+            VALUES ('{p1}','{p2}','{p3}','{p4}','{p5}') RETURNING id"
+            stmt = sql.format(
+                schema=sysmod.schemafin(),
+                p1=account['name'],
+                p2=account['ccy'],
+                p3=account['valid_from'],
+                p4=account['valid_to'],
+                p5=True
+            )
+            cur.execute(stmt)
+            conn.commit()
+            id = cur.fetchone()
+            if id['id'] < 0:
+                    raise psycopg2.OperationalError("Account (id:{0}) creation fail.".format(id))
+            cur.close()
+        return id['id']
+    except psycopg2.OperationalError as err:
+        sysmod.print_data_debug("OperationalError in " + create_accounts.__name__, err)
+        conn.rollback()
+        cur.close()
+        return None
