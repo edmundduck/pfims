faef987efae46de5bb6352c5aa15954417b038e4
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-03-31 19:20:38 +0000

    Edited Routing

--- client_code/Routing.py
+++ client_code/Routing.py
@@ -12,13 +12,15 @@ from anvil.tables import app_tables
 #    Module1.say_hello()
 #
 
-def open_exp_input_form(self):
+def open_exp_input_form(self, **event_args):
     from .Input.ExpenseInputForm import ExpenseInputForm
     self.clear()
-    print("exp", (i.name for i in self.item))
-    self.add_component(ExpenseInputForm())
+    print("exp", (i.text for i in self.item))
+    #self.add_component(ExpenseInputForm())
+    anvil.get_open_form().add_component(ExpenseInputForm())
 
-def open_acct_maint_form(self):
+def open_acct_maint_form(self, **event_args):
     from .Input.AccountMaintForm import AccountMaintForm
     self.clear()
+    print("acct", self.label_1.text)
     self.add_component(AccountMaintForm())
