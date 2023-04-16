d2bebb41b1cc7bf4b82376ab64476204f5c6ad21
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-04-16 23:42:14 +0000

    Edited CashMgtProcess.ExpenseModule

--- server_code/CashMgtProcess/ExpenseModule.py
+++ server_code/CashMgtProcess/ExpenseModule.py
@@ -157,9 +157,11 @@ def delete_transactions(tid, iid_list):
                 cur.execute(stmt)
                 conn.commit()
                 count = cur.rowcount
+                print(cur.statusmessage)
+                print(count)
                 if count <= 0:
                     raise psycopg2.OperationalError("Transactions (tab id:{0}) deletion fail.".format(tid))
-                cur.close()
+                conn.close()
             return count
         return 0
     except psycopg2.OperationalError as err:
