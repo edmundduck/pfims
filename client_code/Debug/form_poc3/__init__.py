9050ea81154fb26c2194f032d4efe34db00ec9ab
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-04-17 22:45:02 +0000

    Edited Debug.form_poc3

--- client_code/Debug/form_poc3/__init__.py
+++ client_code/Debug/form_poc3/__init__.py
@@ -12,3 +12,13 @@ class form_poc3(form_poc3Template):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+
+    def button_1_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        pass
+
+    def button_2_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        pass
+
+
