91e7128910f5651a658d9d65d37b46f8ce296651
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-04-18 21:05:58 +0000

    Created Debug.form_poc3a.RowTemplate3a and Debug.form_poc3a; Edited Debug.form_poc_main

---
+++ client_code/Debug/form_poc3a/RowTemplate3a/__init__.py
@@ -0,0 +1,15 @@
+from ._anvil_designer import RowTemplate3aTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from ....App.Validation import Validator
+
+class RowTemplate3a(RowTemplate3aTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+        # Any code you write here will run before the form opens.
--- client_code/Debug/form_poc_main/__init__.py
+++ client_code/Debug/form_poc_main/__init__.py
@@ -8,6 +8,7 @@ from anvil.tables import app_tables
 from ..form_poc1 import form_poc1
 from ..form_poc2 import form_poc2
 from ..form_poc3 import form_poc3
+from ..form_poc3a import form_poc3a
 from ...App import Global as glo
 
 class form_poc_main(form_poc_mainTemplate):
@@ -21,7 +22,8 @@ class form_poc_main(form_poc_mainTemplate):
         self.drop_down_1.items = [
             ('form_poc1 - Tags', 'form_poc1'),
             ('form_poc2 - Repeating items', 'form_poc2'),
-            ('form_poc3 - Parent & child', 'form_poc3')
+            ('form_poc3 - Parent calls child validation', 'form_poc3'),
+            ('form_poc3a - Child validation done in parent', 'form_poc3a')
         ]
 
     def button_select_click(self, **event_args):
@@ -33,3 +35,5 @@ class form_poc_main(form_poc_mainTemplate):
             self.test_panel.add_component(form_poc2())
         elif self.drop_down_1.selected_value == "form_poc3":
             self.test_panel.add_component(form_poc3())
+        elif self.drop_down_1.selected_value == "form_poc3a":
+            self.test_panel.add_component(form_poc3a())
