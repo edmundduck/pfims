a690e7860fccf6bc12966baea0079c098eeb8847
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-04-22 21:44:18 +0000

    Created Debug.form_poc4; Edited Debug.form_poc_main and custom package dependencies

---
+++ client_code/Debug/form_poc4/__init__.py
@@ -0,0 +1,20 @@
+from ._anvil_designer import form_poc4Template
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import pandas as pd
+
+class form_poc4(form_poc4Template):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+        # Any code you write here will run before the form opens.
+
+    def file_loader_1_change(self, file, **event_args):
+        """This method is called when a new file is loaded into this FileLoader"""
+        df = pd.read_excel(file, sheet_name=None)
+        self.text_area_1.text = df
