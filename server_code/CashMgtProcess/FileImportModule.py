79ec4b4d35f0676f6c68e7ee595bc3ffccf56393
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-06-14 00:02:09 +0000

    Created Input.ExpFileUploadForm.LabelsMappingRPTemplate; Edited CashMgtProcess.FileImportModule and Input.ExpFileUploadForm

--- server_code/CashMgtProcess/FileImportModule.py
+++ server_code/CashMgtProcess/FileImportModule.py
@@ -9,25 +9,17 @@ import pandas as pd
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
+col_name = ['trandate', 'account_id', 'amount', 'remarks', 'stmt_dtl', 'labels']
 
 # Internal function to convert column in character ID to number for Pandas read_excel
 def convertCharToLoc(char):
-    if 'a' <= char.lower() <= 'z':
-            return ord(char.lower()) - 97
-    return None
+    return ord(char.lower()) - 97 if 'a' <= char.lower() <= 'z' else None
 
 # Internal function to get the list of included column names in mapping matrix
 def divMappingColumnNameLists(matrix):
-    col_name = ['trandate', 'account_id', 'amount', 'remarks', 'stmt_dtl', 'labels']
-    nonNanList = []
-    nanList = []
-    for c in col_name:
-        print("c=", c, ", matrix[c]=", matrix[c])
-        if matrix[c] in (None, ''):
-            nanList.append(c)
-        else:
-            nonNanList.append(c)
-    return col_name, nonNanList, nanList
+    nanList, nonNanList = [], []
+    for c in col_name: nanList.append(c) if matrix[c] in (None, '') else nonNanList.append(c)
+    return nonNanList, nanList
 
 @anvil.server.callable
 def preview_file(file):
@@ -37,37 +29,23 @@ def preview_file(file):
 @anvil.server.callable
 def import_file(file, tablist, rules):
     ef = pd.ExcelFile(BytesIO(file.get_bytes()))
-    # df = pd.read_excel(ef, sheet_name=tablist, usecols=rules)
     df = pd.read_excel(ef, sheet_name=tablist)
 
     new_df = None
     for i in rules:
         col = [convertCharToLoc(i['trandate']), convertCharToLoc(i['account_id']), convertCharToLoc(i['amount']),\
                convertCharToLoc(i['remarks']), convertCharToLoc(i['stmt_dtl']), convertCharToLoc(i['labels'])]
-        col_def, nonNanList, nanList = divMappingColumnNameLists(i)
-        # test2 = pd.DataFrame(data=None, columns=nanList)
+        nonNanList, nanList = divMappingColumnNameLists(i)
         for t in tablist:
             # iloc left one is row, right one is column
-            # colobj = {}
-            # for x in nonNanList:
-                # colobj[f"{convertCharToLoc(i[x])}"] = x
-            colobj = dict([(convertCharToLoc(i[x]), x) for x in nonNanList])               
-            print("colobj=", colobj)
-            test1 = df[t].rename(colobj, axis='columns').iloc[:, [x for x in col if x is not None]]
-            test1.loc[:, nanList] = None
-            print("TEST1", test1.to_string())
-            test2 = test1.loc[:, col_def]
-            print("TEST2", test2)
-            # new_df = pd.concat([test1], ignore_index=True) if new_df is None else pd.concat([new_df, test1], ignore_index=True)
-            new_df = pd.concat([test2], ignore_index=True, join="outer") if new_df is None else pd.concat([new_df, test2], ignore_index=True, join="outer")
+            # 1) Filter required columns
+            tmp_df = df[t].iloc[:, [x for x in col if x is not None]]
+            # 2) Rename columns
+            tmp_df = tmp_df.rename(dict([(tmp_df.columns[x], nonNanList[x]) for x in range(len(nonNanList))]), axis='columns')
+            # 3) Add 'not in rule' fields to the end
+            tmp_df.loc[:, nanList] = None
+            # 4) Concat temp DF to the resultant DF
+            new_df = pd.concat([tmp_df.loc[:, col_name]], ignore_index=True, join="outer") if new_df is None else pd.concat([new_df, tmp_df.loc[:, col_name]], ignore_index=True, join="outer")
     # Ref - how to transform Pandas Dataframe to Anvil datatable
     # https://anvil.works/forum/t/add-row-to-data-table/2766/2
-    test = pd.DataFrame(
-        data={"trandate": ["2020-01-02", "2022-05-09"], 
-              "labels": ["801,", "0,"],
-              "amount": [30, 100],
-              "remarks": ["TEST", "AAAG"]
-             }
-    )
     return (new_df.dropna(subset=['amount'], ignore_index=True)).to_dict(orient='records')
-    # return test.to_dict(orient='records')
