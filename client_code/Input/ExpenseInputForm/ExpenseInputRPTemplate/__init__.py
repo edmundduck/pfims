cffc8ad123ca4b78004befde01d5e77aa1fc4d53
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-06-25 19:46:43 +0000

    Edited Input.ExpenseInputForm.ExpenseInputRPTemplate

--- client_code/Input/ExpenseInputForm/ExpenseInputRPTemplate/__init__.py
+++ client_code/Input/ExpenseInputForm/ExpenseInputRPTemplate/__init__.py
@@ -34,35 +34,30 @@ class ExpenseInputRPTemplate(ExpenseInputRPTemplateTemplate):
         if label_list not in ('', None):
             lbls = cache.get_caching_labels_list()
             trimmed_list = label_list[:-1].split(",") if label_list[-1] == ',' else label_list.split(",")
-            for k in trimmed_list:
+            for i in trimmed_list:
                 # Don't generate label if following conditions are met -
                 # 1. label ID is 0 (which is possible from file upload)
                 # 2. label ID is not integer
                 # 3. label ID is NaN
-                i = k.strip()
-                try:
-                    print("1.='", i, "'")
-                    if not math.isnan(i) and int(i) != 0:
-                        print("2.='", i, "'")
-                        lbl_name = None
-                        for j in lbls:
-                            if str(j.get("id")).strip() == i:
-                                lbl_name = j.get("name").strip()
-                        b = Button(text=lbl_name,
-                                # icon='fa:minus',
-                                foreground="White",
-                                background="Blue",
-                                font_size=10,
-                                align="left",
-                                spacing_above="small",
-                                spacing_below="small",
-                                tag=i
-                                )
-                        # self.row_panel_labels.add_component(b, False, name=lbl_name, expand=True)
-                        self.row_panel_labels.add_component(b, False, name=lbl_name)
-                        b.set_event_handler('click', self.label_button_minus_click)
-                except (TypeError) as err:
-                    pass 
+                if i.isdigit() and int(i) != 0:
+                    lbl_name = None
+                    for j in lbls:
+                        if str(j.get("id")).strip() == i:
+                            print(f"j={j.get('id')}+{j.get('name')}/i={i}")
+                            lbl_name = j.get("name").strip()
+                    b = Button(text=lbl_name,
+                            # icon='fa:minus',
+                            foreground="White",
+                            background="Blue",
+                            font_size=10,
+                            align="left",
+                            spacing_above="small",
+                            spacing_below="small",
+                            tag=i
+                            )
+                    # self.row_panel_labels.add_component(b, False, name=lbl_name, expand=True)
+                    self.row_panel_labels.add_component(b, False, name=lbl_name)
+                    b.set_event_handler('click', self.label_button_minus_click)
 
     def label_button_minus_click(self, **event_args):
         b = event_args['sender']
