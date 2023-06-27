afd147c1d2ef21d9be24c4ed02067640766d54a8
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-06-27 23:01:51 +0000

    Edited CashMgtProcess.FileImportModule

--- server_code/CashMgtProcess/FileImportModule.py
+++ server_code/CashMgtProcess/FileImportModule.py
@@ -48,6 +48,10 @@ def import_file(file, tablist, rules, extra):
             # iloc left one is row, right one is column
             # 1) Filter required columns
             tmp_df = df[t].iloc[:, [x for x in col if x is not None]]
+
+            # TODO
+            # Amount scenario
+            tmp_df = tmp_df[]
             # 2) Rename columns
             tmp_df = tmp_df.rename(dict([(tmp_df.columns[x], nonNanList[x]) for x in range(len(nonNanList))]), axis='columns')
             # 3) Add 'not in rule' fields to the end
