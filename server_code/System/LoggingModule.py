02e143701d6c3ea435d7fc2cf1a75cb00f6d4941
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-07-06 09:52:55 +0000

    Created System.LoggingModule

---
+++ server_code/System/LoggingModule.py
@@ -0,0 +1,10 @@
+import anvil.secrets
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+
