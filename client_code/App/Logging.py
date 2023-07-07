7e846777d3e0ce329a2e2ff0016ed996a2d11030
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-07-07 10:15:34 +0000

    Edited App.Logging

--- client_code/App/Logging.py
+++ client_code/App/Logging.py
@@ -7,6 +7,7 @@ import datetime
 # This is a module.
 # You can define variables and functions here, and use them from any form. For example, in a top-level form:
 
+# Constants
 DEBUG = 'DEBUG'
 INFO = 'INFO'
 WARNING = 'WARNING'
@@ -14,13 +15,15 @@ ERROR = 'ERROR'
 CRITICAL = 'CRITICAL'
 
 class Logger():
-    def __init__(self, config, level=INFO):
+    def __init__(self, config, level=INFO, enabled=True):
         self.datefmt = config.datefmt
         self.level = level
+        self.enabled = enabled
 
     def log(self, message):
-        current = datetime.datetime.now()
-        print(f"{current.strftime(self.datefmt)} - {self.level} - {message}")
+        if self.enabled:
+            current = datetime.datetime.now()
+            print(f"{current.strftime(self.datefmt)} - {self.level} - {message}")
 
 class LoggerConfig():
     def __init__(self, datefmt='%Y-%m-%d %H:%M:%S'):
@@ -28,8 +31,8 @@ class LoggerConfig():
 
 config = LoggerConfig()
 
-debug = Logger(config, DEBUG)
-info = Logger(config)
-warning = Logger(config, WARNING)
-error = Logger(config, ERROR)
-critical = Logger(config, CRITICAL)
+debug = Logger(config=config, level=DEBUG, enabled=False)
+info = Logger(config=config)
+warning = Logger(config=config, level=WARNING)
+error = Logger(config=config, level=ERROR)
+critical = Logger(config=config, level=CRITICAL)
