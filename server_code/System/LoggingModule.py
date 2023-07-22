df3be500248c9260fae8dfd5dc68cb9d2545ffb3
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-07-22 23:37:58 +0000

    Edited System.LoggingModule

--- server_code/System/LoggingModule.py
+++ server_code/System/LoggingModule.py
@@ -15,25 +15,24 @@ import datetime
 DEBUG_LARGEDATA = {'val':logging.DEBUG-5, 'desc':'DUMP'}
 
 LOGGING_CONFIG = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "standard": {
-                "format": "[S] %(asctime)s [%(levelname)] %(message)s"
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': '[S] %(asctime)s [%(levelname)] %(message)s'
             }
         },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "level": "DEBUG",
-                "formatter": "standard",
-                "stream": "ext://sys.stdout"
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard',
+                'stream': 'ext://sys.stdout'
             }
         },
-        "loggers": {
-            "": {
-                "level": "DEBUG",
-                "handlers": ["console"]
+        'loggers': {
+            '': {
+                'level': 'DEBUG',
+                'handlers': ['console']
             }
         }
     }
@@ -49,10 +48,9 @@ class ServerLogger():
         current = datetime.datetime.now()
         level = self.level.get('val') if isinstance(self.level, dict) else self.level
         output = f" [{self.level.get('desc')}] {msg} " if isinstance(self.level, dict) else f" {msg} "
-        if len(args) > 0: output = "{a} {b}".format(a=output, b=args)
-        if len(kwargs) > 0: output = "{a} {b}".format(a=output, b=kwargs)
+        if len(args) > 0: output = '{a} {b}'.format(a=output, b=args)
+        if len(kwargs) > 0: output = '{a} {b}'.format(a=output, b=kwargs)
         if level == logging.DEBUG:
-            print(f"TESTING {output}")
             self.logger.debug(output)
         elif level == logging.INFO:
             self.logger.info(output)
