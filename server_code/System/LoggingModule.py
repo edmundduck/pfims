0ae363221e335454186618109e7ae202506dcb07
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-07-27 18:42:45 +0000

    Edited System.LoggingModule

--- server_code/System/LoggingModule.py
+++ server_code/System/LoggingModule.py
@@ -41,31 +41,30 @@ class ServerLoggerConfig:
         }
 
 class ServerLogger:
-    logger = None
+    logger = logging.getLogger(__name__)
     
     def __init__(self, config=ServerLoggerConfig.DEFAULT_LOGGING_CONFIG, level=get_user_logging_level()):
         logging.addLevelName(ServerLoggerLevel.TRACE.get('val'), ServerLoggerLevel.TRACE.get('desc'))
         logging.config.dictConfig(config)
-        logger = logging.getLogger(__name__)
-        logger.setLevel(level if level is not None else ServerLoggerLevel.DEFAULT_LVL)
+        ServerLogger.logger.setLevel(level if level is not None else ServerLoggerLevel.DEFAULT_LVL)
         
     def trace(self, msg=None, *args, **kwargs):
-        logger._log(ServerLoggerLevel.TRACE.get('val'), msg, args, **kwargs)
+        ServerLogger.logger._log(ServerLoggerLevel.TRACE.get('val'), msg, args, **kwargs)
 
     def debug(self, msg=None, *args, **kwargs):
-        logger.debug(msg, args, **kwargs)
+        ServerLogger.logger.debug(msg, args, **kwargs)
 
     def info(self, msg=None, *args, **kwargs):
-        logger.info(msg, args, **kwargs)
+        ServerLogger.logger.info(msg, args, **kwargs)
 
     def warning(self, msg=None, *args, **kwargs):
-        logger.warning(msg, args, **kwargs)
+        ServerLogger.logger.warning(msg, args, **kwargs)
 
     def error(self, msg=None, *args, **kwargs):
-       logger.error(msg, args, **kwargs)
+        ServerLogger.logger.error(msg, args, **kwargs)
 
     def critical(self, msg=None, *args, **kwargs):
-        logger.critical(msg, args, **kwargs)
+        ServerLogger.logger.critical(msg, args, **kwargs)
 
 def log_function(func):
     def wrapper(*args, **kwargs):
