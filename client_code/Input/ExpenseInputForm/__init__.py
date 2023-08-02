0e89adf6765b6c60cd3a665318eea5e9732229e4
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-08-02 14:24:07 +0000

    Edited Input.ExpenseInputForm

--- client_code/Input/ExpenseInputForm/__init__.py
+++ client_code/Input/ExpenseInputForm/__init__.py
@@ -232,5 +232,5 @@ class ExpenseInputForm(ExpenseInputFormTemplate):
         self._switch_to_save_button()
 
     def reload_rp_data(self, **event_args):
-        for d in self.repeating_panel_1.get_components(): logger.trace("reload_rp_data d.item=", d.item)
-        self.repeating_panel_1.items = [c.item for c in self.repeating_panel_1.get_components() if c.item['id'] not in cache.get_deleted_row()]
+        for d in self.input_repeating_panel.get_components(): logger.trace("reload_rp_data d.item=", d.item)
+        self.input_repeating_panel.items = [c.item for c in self.input_repeating_panel.get_components() if c.item['iid'] not in cache.get_deleted_row()]
