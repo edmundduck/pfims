e802173c766d9f5c2aa64862f169d530e224b6f3
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-08-23 19:42:26 +0000

    Edited Input.ExpenseFilePDFImportForm.PDFColumnsMappingRPTemplate and Input.ExpenseFilePDFImportForm

--- client_code/Input/ExpenseFilePDFImportForm/PDFColumnsMappingRPTemplate/__init__.py
+++ client_code/Input/ExpenseFilePDFImportForm/PDFColumnsMappingRPTemplate/__init__.py
@@ -17,32 +17,13 @@ class PDFColumnsMappingRPTemplate(PDFColumnsMappingRPTemplateTemplate):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
-        self.dropdown_lbl_action.items = cache.labels_mapping_action_dropdown()
-        self.dropdown_lbl_map_to.items = cache.labels_dropdown()
-        self.hidden_lbl_action.text = None
+        self.dropdown_col_map_to.items = cache.expense_tbl_def_dropdown()
+        self.dropdown_sign.visible = False
 
-        # Prefill "labels map to" dropdown by finding high proximity choices
-        self.dropdown_lbl_map_to.selected_value = repr(self.item['tgtlbl']) if self.item['tgtlbl'] is not None else None
-
-    def dropdown_lbl_action_show(self, **event_args):
-        """This method is called when the DropDown is shown on the screen"""
-        action, action_desc = self.dropdown_lbl_action.selected_value if self.dropdown_lbl_action.selected_value is not None else [None, None]
-        if action in (None, const.FileImportLabelExtraAction.SKIP):
-            self.dropdown_lbl_map_to.visible = False
-            self.input_label.visible = False
-        elif action == const.FileImportLabelExtraAction.MAP:
-            self.dropdown_lbl_map_to.visible = True
-            self.input_label.visible = False
-        elif action == const.FileImportLabelExtraAction.CREATE:
-            self.dropdown_lbl_map_to.visible = False
-            self.input_label.visible = True
-            self.parent.raise_event('x-refresh-label-cache')
-
-    @logger.log_function
-    def dropdown_lbl_action_change(self, **event_args):
+    def dropdown_col_map_to_change(self, **event_args):
         """This method is called when an item is selected"""
-        self.dropdown_lbl_action_show()
-        action, action_desc = self.dropdown_lbl_action.selected_value if self.dropdown_lbl_action.selected_value is not None else [None, None]
-        prev = self.hidden_lbl_action.text
-        self.hidden_lbl_action.text = action
-        self.parent.raise_event('x-handle-action-count', action=action, prev=prev)
+        if self.dropdown_col_map_to.selected_value == const.xxx:
+            self.dropdown_sign.visible = True
+        else:
+            self.dropdown_sign.visible = False
+
