ab1900d006fdfae9cf915666acd7f822c9b43ad2
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-08-23 22:03:34 +0000

    Edited CashMgtProcess.FileImportModule and Input.ExpenseFilePDFImportForm

--- client_code/Input/ExpenseFilePDFImportForm/__init__.py
+++ client_code/Input/ExpenseFilePDFImportForm/__init__.py
@@ -24,17 +24,16 @@ class ExpenseFilePDFImportForm(ExpenseFilePDFImportFormTemplate):
         # Transpose Dict of Lists (DL) to List of Dicts (LD)
         # Ref - https://stackoverflow.com/questions/37489245/transposing-pivoting-a-dict-of-lists-in-python
         DL = {
-            'srclbl': labels,
-            'action': [ None for i in range(len(labels))],
-            'tgtlbl': relevant_lbls,
-            'new': labels
+            'srccol': data[0],
+            'tgtcol': [],
+            'sign': []
         }
         logger.trace("DL=", DL)
-        self.labels_mapping_panel.items = [dict(zip(DL, col)) for col in zip(*DL.values())]
-        logger.trace("self.labels_mapping_panel.items=", self.labels_mapping_panel.items)
-        self.hidden_action_count.text = len(labels)
-        self.labels_mapping_panel.add_event_handler('x-handle-action-count', self.handle_action_count)
-        self.labels_mapping_panel.add_event_handler('x-refresh-label-cache', self.handle_action_count)
+        self.cols_mapping_panel.items = [dict(zip(DL, col)) for col in zip(*DL.values())]
+        logger.trace("self.cols_mapping_panel.items=", self.cols_mapping_panel.items)
+        # self.hidden_action_count.text = len(labels)
+        # self.cols_mapping_panel.add_event_handler('x-handle-mapping-count', self.handle_mapping_count)
+        # self.cols_mapping_panel.add_event_handler('x-refresh-label-cache', self.handle_action_count)
 
     def button_nav_upload_mapping_click(self, **event_args):
         """This method is called when the button is clicked"""
@@ -53,14 +52,14 @@ class ExpenseFilePDFImportForm(ExpenseFilePDFImportFormTemplate):
     @logger.log_function
     def button_next_click(self, **event_args):
         """This method is called when the button is clicked"""
-        df = anvil.server.call('update_mapping', data=self.tag.get('data'), mapping=self.labels_mapping_panel.items)
+        df = anvil.server.call('update_mapping', data=self.tag.get('data'), mapping=self.cols_mapping_panel.items)
         Routing.open_exp_input_form(self, tab_id=self.dropdown_tabs.selected_value, data=df)
 
-    def handle_action_count(self, action, prev, **event_args):
+    def handle_mapping_count(self, action, prev, **event_args):
         if action is None:
-            self.hidden_action_count.text = int(self.hidden_action_count.text) + 1
+            self.hidden_mapping_count.text = int(self.hidden_mapping_count.text) + 1
         elif prev is None:
-            self.hidden_action_count.text = int(self.hidden_action_count.text) - 1
+            self.hidden_mapping_count.text = int(self.hidden_mapping_count.text) - 1
         else:
             pass
         self.enable_next_button()
