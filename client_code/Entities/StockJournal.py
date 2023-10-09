77875f2791defdec4644bcf256938d5b18883bc3
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-10-09 08:07:04 +0000

    Created Entities.StockJournalGroup; Edited Entities.StockJournal, Entities.Label and Entities.Account

--- client_code/Entities/StockJournal.py
+++ client_code/Entities/StockJournal.py
@@ -1,8 +1,5 @@
 import anvil.server
 import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 import datetime as datetime
 # This is a module.
 # You can define variables and functions here, and use them from any form. For example, in a top-level form:
--- client_code/Entities/StockJournalGroup.py
+++ client_code/Entities/StockJournalGroup.py
@@ -3,23 +3,25 @@ import anvil.users
 # This is a module.
 # You can define variables and functions here, and use them from any form. For example, in a top-level form:
 
-class Account:
+class StockJournalGroup:
     def __init__(self, data):
         self.set(data)
 
     def get(self):
-        return [self.id, self.name, self.ccy, self.valid_from, self.valid_to, self.status]
+        return [self.id, self.name, self.broker_id, self.submitted, self.created_time, self.lastsaved_time, self.submitted_time, self.journals]
         
     def set(self, data):
         if data and isinstance(data, list):
             self.id = data[0]
             self.name = data[1]
-            self.ccy = data[2]
-            self.valid_from = data[3]
-            self.valid_to = data[4]
-            self.status = data[5]
+            self.broker_id = data[2]
+            self.submitted = data[3]
+            self.created_time = data[4]
+            self.lastsaved_time = data[5]
+            self.submitted_time = data[6]
+            self.journals = data[7]
 
     def is_valid(self):
         if not self.name or self.name.isspace(): return False
-        if not self.ccy or self.ccy.isspace(): return False
+        if not self.broker_id or self.broker_id.isspace(): return False
         return True
