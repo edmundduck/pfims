276d44d82f88e9a472c6da491050c70a008b0187
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-10-10 08:11:04 +0000

    Edited Entities.Setting

--- client_code/Entities/Setting.py
+++ client_code/Entities/Setting.py
@@ -5,6 +5,8 @@ import anvil.users
 
 @anvil.server.portable_class
 class Setting:
+    __column_def__ = ['userid', 'default_broker', 'default_interval', 'default_datefrom', 'default_dateto', 'logging_level']
+    
     def __init__(self, data):
         self.set(data)
 
@@ -18,7 +20,14 @@ class Setting:
             self.logging_level)
 
     def get(self):
-        return [self.default_broker, self.default_interval, self.default_datefrom, self.default_dateto, self.logging_level]
+        return {
+            self.__column_def__[0]: self.userid,
+            self.__column_def__[1]: self.default_broker,
+            self.__column_def__[2]: self.default_interval,
+            self.__column_def__[3]: self.default_datefrom,
+            self.__column_def__[4]: self.default_dateto,
+            self.__column_def__[5]: self.logging_level
+        }
 
     def get_broker(self):
         return self.default_broker
@@ -36,13 +45,13 @@ class Setting:
         return self.logging_level
 
     def set(self, data):
-        if data:
+        if data and isinstance(data, dict):
             self.userid = data.get('userid')
             self.default_broker = data.get('default_broker')
             self.default_interval = data.get('default_interval')
             self.default_datefrom = data.get('default_datefrom')
             self.default_dateto = data.get('default_dateto')
-            self.logging_level = listdata.get('logging_level')
+            self.logging_level = data.get('logging_level')
 
     def is_valid(self):
         return True
@@ -52,5 +61,5 @@ class Setting:
         return self.userid
 
     def __deserialize__(self, userid, global_data):
-        data = [userid] + global_data[f"Setting_{userid}"]
+        data = global_data[f"Setting_{userid}"]
         self.__init__(data)
