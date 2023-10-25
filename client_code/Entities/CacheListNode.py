8a0334aef4d903a624a4f14fac2f81abfbcb39b7
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-10-25 18:53:29 +0000

    Edited Entities.CacheListNode

--- client_code/Entities/CacheListNode.py
+++ client_code/Entities/CacheListNode.py
@@ -15,11 +15,17 @@ class Node:
     def get_prev(self):
         return self.prev
 
+    def get_value(self):
+        return self.data
+
     def set_next(self, data):
         self.next = Node(data)
 
     def set_prev(self, data):
         self.prev = Node(data)
+
+    def set_value(self, data):
+        self.data = data
 
 class DoubleLinkedList:
     def __init__(self):
@@ -47,4 +53,40 @@ class DoubleLinkedList:
             self.head = new_node
             self.tail = new_node
             new_node.set_next(None)
-        
+
+    def remove_from_head(self):
+        unwanted_node = self.head
+        if unwanted_node:
+            self.head = unwanted_node.get_next()
+            unwanted_node.next = None
+        if self.head:
+            self.head.prev = None
+        return unwanted_node.get_value()
+
+    def remove_from_tail(self):
+        unwanted_node = self.tail
+        if unwanted_node:
+            self.tail = unwanted_node.get_prev()
+            unwanted_node.prev = None
+        if self.tail:
+            self.tail.next = None
+        return unwanted_node.get_value()
+
+    def pop(self, data):
+        node_to_search = this.head
+        while node_to_search:
+            if node_to_search.get_value() == data:
+                if not node_to_search.get_prev():
+                    return self.remove_from_head()
+                elif not node_to_search.get_next():
+                    return self.remove_from_tail()
+                else:
+                    prev_node = node_to_search.get_prev()
+                    next_node = node_to_search.get_next()
+                    prev_node.set_next(next_node)
+                    next_node.set_prev(prev_node)
+                    node_to_search.set_prev(None)
+                    node_to_search.set_next(None)
+                    return node_to_search.get_value()
+            else:
+                node_to_search = node_to_search.get_next()
