b81723612c6cb176bfa3fb42d74e2874afdd1851
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-11-05 18:28:25 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,5 +11,4 @@ class Form1(Form1Template):
         # Any code you write here will run when the form opens.
         from ..Forms.Expense.ExpenseReportSearchPanelForm import ExpenseReportSearchPanelForm
         from ..Forms.Expense.ExpenseAnalysisForm import ExpenseAnalysisForm
-        self.content_panel1.width = 1800
-        self.content_panel1.add_component(ExpenseReportSearchPanelForm(ExpenseAnalysisForm(), full_width_row=True))
+        self.open_form(ExpenseReportSearchPanelForm(ExpenseAnalysisForm()))
