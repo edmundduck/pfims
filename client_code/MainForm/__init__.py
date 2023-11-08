d3a507bb90fa5fd5f67f3e5e8caadb7c73d5c713
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-11-08 11:30:53 +0000

    Edited MainForm

--- client_code/MainForm/__init__.py
+++ client_code/MainForm/__init__.py
@@ -5,15 +5,22 @@ from ..Controllers import MainFormController
 from ..Utils import Routing
 
 class MainForm(MainFormTemplate):
+    investment_visgrp = []
+    expense_visgrp = []
+    
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
         
         # Any code you write here will run when the form opens.
         from .. import Global
+        self.investment_visgrp = [self.colpanel_link_input_stock.visible, self.colpanel_link_tranx_list.visible, self.colpanel_link_pnl_report.visible]
+        self.expense_visgrp = [self.colpanel_link_input_exp.visible, self.colpanel_link_exp_list.visible, self.colpanel_link_exp_analysis.visible]
         self.app_welcome_msg.text = "Welcome {username}".format(username=Global.email)
         self.label_version.text = MainFormController.visible_test_env_label()
         self.poc_link.visible = MainFormController.visible_poc_link()
+        self.investment_visgrp = [False] *3
+        self.expense_visgrp = [False] *3
 
     def reset_link_status(self, **event_args):
         self.colpanel_link_input_stock.role, \
@@ -77,6 +84,18 @@ class MainForm(MainFormTemplate):
         self.reset_link_status()
         self.poc_link.role = MainFormController.switch_role(self.poc_link.role)
         Routing.open_poc_main_form(self)
+
+    def colpanel_link_investment_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        self.reset_link_status()
+        self.colpanel_link_input_stock.visible, self.colpanel_link_tranx_list.visible, self.colpanel_link_pnl_report.visible = [True] *3
+        self.colpanel_link_input_exp.visible, self.colpanel_link_exp_list.visible, self.colpanel_link_exp_analysis.visible = [False] *3
+
+    def colpanel_link_expense_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        self.reset_link_status()
+        self.colpanel_link_input_stock.visible, self.colpanel_link_tranx_list.visible, self.colpanel_link_pnl_report.visible = [False] *3
+        self.colpanel_link_input_exp.visible, self.colpanel_link_exp_list.visible, self.colpanel_link_exp_analysis.visible = [True] *3
 
     def app_logout_click(self, **event_args):
         """This method is called when the link is clicked"""
