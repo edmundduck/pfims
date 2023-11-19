5aab35e6773947aaca88add041a000477de7db0c
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2023-11-19 00:25:49 +0000

    Edited Error.ValidationError and BusinessLogic.FileImportMappingBLModule

--- client_code/Error/ValidationError.py
+++ client_code/Error/ValidationError.py
@@ -5,11 +5,12 @@ import anvil.server
 
 # Ref - https://anvil.works/forum/t/handling-exception-coming-from-the-server-uplink-modules/1400
 class ValidationError(anvil.server.AnvilWrappedError):
-    def __init__(self, message, *args):
-        self.message = message
-        self.args = args
+    # def __init__(self, message, *args):
+    #     self.message = message
+    #     self.args = args
     
-    def __str__(self):
-        return "[{}] {} {}".format(type(self).__name__, self.message, " ".join(self.args))
-
+    # def __str__(self):
+    #     return "[{}] {} {}".format(type(self).__name__, self.message, " ".join(self.args))
+    pass
+    
 anvil.server._register_exception_type('ValidationError', ValidationError)
