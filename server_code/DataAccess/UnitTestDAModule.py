ce91925ca2a9aa8b1d2389c4933796f55b4f6b04
edomondo.shima@gmail.com <edomondo.shima@gmail.com>
2024-03-31 19:00:04 +0000

    Edited DataAccess.UnitTestDAModule

--- server_code/DataAccess/UnitTestDAModule.py
+++ server_code/DataAccess/UnitTestDAModule.py
@@ -1,16 +1,11 @@
 import anvil.files
 from anvil.files import data_files
 import anvil.server
-import os
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 
 @anvil.server.callable
 def access_unit_test_data():
-    unittest_dir = data_files['unittest']
-
-    with os.scandir(unittest_dir) as directory:
-        for file in directory:
-            if not file.name.startswith('.') and file.is_file():
-                print(file.name)
+    with open(data_files['load_unittest_data.sql']) as f:
+        print(f"{list(line.strip() for line in f)}")
